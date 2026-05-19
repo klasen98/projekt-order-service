c9dc71eec38a8e9c6ad629ca36323dbc4c6e80be
klasen98 <klasen98@live.se>
2026-05-19 13:16:53 +0200

    Initial commit

---
+++ src/main/java/se/iths/jakob/projektorderservice/ProjektOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.jakob.projektorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektOrderServiceApplication.class, args);
+    }
+
+}
