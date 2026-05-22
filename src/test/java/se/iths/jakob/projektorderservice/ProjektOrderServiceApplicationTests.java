28d6053b78c3f17bc57817bb5bcaa63871cbed5d
klasen98 <klasen98@live.se>
2026-05-22 11:18:26 +0200

    controller och service

--- src/test/java/se/iths/jakob/projektorderservice/ProjektOrderServiceApplicationTests.java
+++ src/test/java/se/iths/jakob/projektorderservice/ProjektOrderServiceApplicationTests.java
@@ -1,20 +1,12 @@
 package se.iths.jakob.projektorderservice;
 
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ProjektOrderServiceApplicationTests {
 
-    @MockitoBean
-    private JwtDecoder jwtDecoder;
-
-    @MockitoBean
-    private ConnectionFactory connectionFactory;
 
     @Test
     void contextLoads() {
